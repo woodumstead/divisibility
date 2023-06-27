418a86ee7f6518f60472ff1c6846cc76a85134e3
Woody Umstead <76594506+woodumstead@users.noreply.github.com>
2023-06-26 21:56:27 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+// Using an if-else statement, determine if a number is divisible by 3, by 5 or both.
+
+let q1test = 15;
+
+if (q1 test % 3 === 0 && % 5 ===0) {
+    console.log("divisible by both!");
+} else if (q1test % 5 === 0) {
+    console.log("divisible by 5");
+} else if {q1 test % 3 === 0) {
+    console.log(divisible by 3");
+} else {
+  console.log("not divisible by 3, 5 or both");
